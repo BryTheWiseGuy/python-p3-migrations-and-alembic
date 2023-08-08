2f554af7474a2e08838b2eae53ce9a4725129016
Brian Ashman <brian.ashman@comcast.net>
2023-08-07 19:56:11 -0700

    Completed Code Along

---
+++ lib/migrations/versions/de7c2444a319_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: de7c2444a319
+Revises: 
+Create Date: 2023-08-07 18:30:50.213626
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'de7c2444a319'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
